f7845d597a3c38370d31815ed982c66485ebefd2
Hexau <ivanhxgomez77@gmail.com>
2022-01-20 15:50:11 -0600

    Nombre del proyecto: my-app, Descripcion: Version final del proyecto my app

---
+++ src/main/java/com/mDevhX/myapp/entity/TestEntity.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.mDevhX.myapp.entity;
+
+/**
+ * @author Hexau
+ *
+ */
+public class TestEntity {
+	/**
+	 * Propiedad de prueba para la muestra de copilacion del archivo testentity.
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
